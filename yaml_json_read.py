2bdc493be112a0f605f8332a339600ba127da245
TTLequals0 <dkrachtus@ttlequals0.com>
2016-08-13 16:20:29 -0500

    added yaml_json_read.py

---
+++ yaml_json_read.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+Write a Python program that reads both the YAML file and the JSON file created
+in exercise6 and pretty prints the data structure that is returned.
+'''
+
+import yaml
+import json
+
+
+from pprint import pprint 
+
+def output_format(my_list, my_str):
+	'''
+	Make output easier to read
+	'''
+
+	print '\n\n'
+	print '#' * 3
+	print '#' * 3 + my_str
+	print '#' * 3 
+	pprint(my_list)
+
+def main():
+	'''
+	Read yaml and JSON files, Pretty print to standard out.
+	'''
+
+	yaml_file = 'output.yml'
+	json_file = 'output.json'
+
+	with open(yaml_file) as f:
+		yaml_list = yaml.load(f)
+
+	with open(json_file) as f:
+		json_list = json.load(f)
+
+	output_format(yaml_list, 'YAML')
+	output_format(json_list, 'JSON')
+	print '\n'
+
+if __name__ ==  "__main__":
+	main()
